722df35a305b68f5ce4fdcca191a1ca86ec91876
Alex Korobkov <dreissena85@mail.ru>
2023-02-24 12:55:58 +0300

    телеграм бот "Камень, ножницы, бумага, ящерица, спок". Готовый бот.

--- First/Telebot.py
+++ First/Telebot.py
@@ -1,1 +1,47 @@
-print("Hello world")
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from handlers import other_handlers, user_handlers
+from config.config import Config, load_config
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config(None)
+    API_TOKEN: str = config.tg_bot.token
+
+    # Создаем объекты бота и диспетчера
+    bot: Bot = Bot(token=API_TOKEN, parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        # Запускаем функцию main в асинхронном режиме
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        # Выводим в консоль сообщение об ошибке,
+        # если получены исключения KeyboardInterrupt или SystemExit
+        logger.error('Bot stopped!')
