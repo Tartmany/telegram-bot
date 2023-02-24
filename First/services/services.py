715bbd82168f01fb3cb73228df1456d6df73b125
Alex Korobkov <dreissena85@mail.ru>
2023-02-24 12:57:42 +0300

    телеграм бот "Камень, ножницы, бумага, ящерица, спок". Готовый бот.

---
+++ First/services/services.py
@@ -0,0 +1,33 @@
+import random
+
+from lexicon.lexicon_ru import LEXICON_RU
+
+
+# Функция, возвращающая случайный выбор бота в игре
+def get_bot_choice() -> str:
+    return random.choice(['rock', 'paper', 'scissors', 'lizard', 'spock'])
+
+
+# Функция, возвращающая ключ из словаря, по которому
+# хранится значение, передаваемое как аргумент - выбор пользователя
+def _normalize_user_answer(user_answer: str) -> str:
+    for key in LEXICON_RU:
+        if LEXICON_RU[key] == user_answer:
+            return key
+    raise Exception
+
+
+# Функция, определяющая победителя
+def get_winner(user_choice: str, bot_choice: str) -> str:
+    user_choice = _normalize_user_answer(user_choice)
+    rules: dict[str, list] = {'rock': ['scissors', 'lizard'],
+                              'scissors': ['paper', 'lizard'],
+                              'paper': ['rock', 'spock'],
+                              'lizard': ['spock', 'paper'],
+                              'spock': ['scissors', 'rock']}
+    if user_choice == bot_choice:
+        return 'nobody_won'
+    elif bot_choice in rules[user_choice]:
+        return 'user_won'
+    else:
+        return 'bot_won'
